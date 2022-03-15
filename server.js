1be73d102c33f889a7f61c6934e2b8eaa9ae8a5f
Santoshkumar S <sskumar1999422gmail.com>
2022-03-15 22:14:19 +0530

    Checkpoint 6

--- server.js
+++ server.js
@@ -1,42 +1,58 @@
-var express = require('express')
-  ,http = require('http')
-  ,fs = require('fs')
-  ,path = require('path');
+var express = require("express"),
+  http = require("http"),
+  fs = require("fs"),
+  path = require("path"),
+  cors = require("cors");  //TODO:
+
+
+
+// Running on posrt 9000
+const PORT = 3000;
+
 
 var app = express();
+
+var corsOptions = {
+  origin: "http://localhost:"+PORT
+};
+
+app.use(cors(corsOptions));
 
 const db = require("./models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database!");
-    
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
 
-app.get('/movie',function(req,res){
-    res.send("All Movies Data in JSON format from Mongo DB")
-})
+// app.get("/movie", findAllMovies);
+// app.get('',findOne);
 
-app.get('/genres',function(req,res){
-    res.send("All Genres Data in JSON format from Mongo DB")
-})
+// app.get('/genres',function(req,res){
+//     res.send("All Genres Data in JSON format from Mongo DB")
+// })
 
-app.get('/artists',function(req,res){
-    res.send("All Artists Data in JSON format from Mongo DB")
-})
+// app.get('/artists',function(req,res){
+//     res.send("All Artists Data in JSON format from Mongo DB")
+// })
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Upgrad Movie booking application development.",
+  });
+});
 
+require("./routes/movie.routes")(app);
+require("./routes/artist.routes")(app);
+require("./routes/genre.routes")(app);
+// require("./routes/user.routes")(app)
 
-
-app.listen(9000, function () {
-    console.log("express has started on port 3000");
-   });
-   
-
-  
+app.listen(PORT, function () {
+  console.log("express has started on port ", PORT);
+});
